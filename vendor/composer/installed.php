419764c6350cfdefcb98b5b8f319598579e584a0
willidevv <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>
2026-06-02 20:14:38 +0700

    update composer

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'iqbal/toko-online-pjbl',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '07754bb711a052bacdfa1c8c5d31b49d5a0a92b5',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'iqbal/toko-online-pjbl' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '07754bb711a052bacdfa1c8c5d31b49d5a0a92b5',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
